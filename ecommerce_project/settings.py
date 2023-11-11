bd453d6e1221e303cd5fdb5718be97e4441216d9
shinhosuck <shinhosuck1973@gmail.com>
2023-11-11 20:43:57 +0800

    update

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.255.37', 'aiaimarket.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.15.37', 'aiaimarket.pythonanywhere.com']
 # ALLOWED_HOSTS = []
 
 # Application definition
