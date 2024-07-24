a9b09de2ff8768309a85ce4c1d66f32bb02b2daf
shinhosuck <shinhosuck1973@gmail.com>
2024-07-24 13:53:33 +0800

    update

--- products/views.py
+++ products/views.py
@@ -356,9 +356,6 @@ def checkout_summary_view(request):
     orders = Cart.objects.filter(customer=user, open=True)
     address = user.addresses.first()
 
-    # if not orders.exists():
-    #     messages.error(request, 'You basket is empty. Please add a product to your basket and try again.')
-    #     return redirect('products:product-cart')
     if not address:
         messages.warning(request, f'{user.username}, please add your shipping address!')
         return redirect('products:shipping-address')
